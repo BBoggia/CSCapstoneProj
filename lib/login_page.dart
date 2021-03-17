7460229e61ed0a4d34e45dbf79e64715cffb8715
ltoscano97 <ltoscano@kent.edu>
2021-03-17 17:36:03 -0400

    removed hello

---
+++ lib/login_page.dart
@@ -0,0 +1,9 @@
+class Authentication {
+  static Future<FirebaseApp> initializeFirebase() async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+
+    // TODO: Add auto login logic
+
+    return firebaseApp;
+  }
+}

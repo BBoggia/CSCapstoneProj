b4cc6d94b4f7efeb5656eb9babd24b40b2b1d19b
ltoscano97 <ltoscano@kent.edu>
2021-03-19 12:46:49 -0400

    modified main

--- lib/login_page.dart
+++
@@ -1,9 +0,0 @@
-class Authentication {
-  static Future<FirebaseApp> initializeFirebase() async {
-    FirebaseApp firebaseApp = await Firebase.initializeApp();
-
-    // TODO: Add auto login logic
-
-    return firebaseApp;
-  }
-}

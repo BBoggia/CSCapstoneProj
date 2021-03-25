65fd42fac975f661a8931f1a0f063c8453464113
ltoscano97 <ltoscano@kent.edu>
2021-03-25 17:14:51 -0400

    trying to add speech to text

--- lib/journal_input.dart
+++ lib/journal_input.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 class Today {
   DateTime now;
@@ -16,26 +17,68 @@ class Today {
   }
 }
 
-class JournalEntry extends StatelessWidget {
+class JournalEntry extends StatefulWidget {
+  @override
+  _SpeechScreenState createState() => _SpeechScreenState();
+}
+
+class _SpeechScreenState extends State<JournalEntry> {
+  stt.SpeechToText _speech;
+  bool _isListening = false;
+  String text = 'Press the button and start speaking';
+  double _confidence = 1.0;
+  var txt = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _speech = stt.SpeechToText();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: Column(children: [
-        SizedBox(
-          height: 50.0,
-        ),
-        Text("Journal Entry for: " + Today().formattedDate,
-            style: TextStyle(fontSize: 25)),
-        TextField(
-          keyboardType: TextInputType.multiline,
-          minLines: 5,
-          maxLines: null,
-          obscureText: false,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'Journal Entry',
+    return Scaffold(
+      appBar: AppBar(
+        title:
+            Text('confidence is: ${(_confidence * 100.0).toStringAsFixed(1)}%'),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+          onPressed: _listen,
+          child: Icon(_isListening ? Icons.mic : Icons.mic_none)),
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 150.0),
+          child: TextField(
+            controller: txt,
           ),
         ),
-      ]),
+      ),
     );
+  }
+
+  void _listen() async {
+    if (!_isListening) {
+      bool avaliable = await _speech.initialize(
+        onStatus: (val) => print('onStatus: $val'),
+        onError: (val) => print('onError: $val'),
+      );
+      if (avaliable) {
+        setState(() => _isListening = true);
+        _speech.listen(
+          onResult: (val) => setState(() {
+            txt.text = val.recognizedWords;
+            print(val.recognizedWords);
+            if (val.hasConfidenceRating && val.confidence > 0) {
+              _confidence = val.confidence;
+            }
+          }),
+        );
+      }
+    } else {
+      setState(() => _isListening = false);
+      _speech.stop();
+    }
   }
 }

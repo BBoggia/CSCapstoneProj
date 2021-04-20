f8d40504ca83aa54f2e84ffe68917a7a1c074160
ltoscano97 <ltoscano@kent.edu>
2021-04-20 15:56:09 -0400

    Hard coded horoscope logic, needs to be added db

--- lib/authentication.dart
+++ lib/authentication.dart
@@ -3,6 +3,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter/material.dart';
+import 'package:horoscope/horoscope_flutter.dart';
 
 /*
 * This is the authentication API
@@ -91,5 +92,107 @@ class Authentication {
     final googleSignIn = GoogleSignIn(); // get current sign in
     await googleSignIn.signOut(); //sign it out from google
     await _firebaseAuth.signOut(); // sign out from firebase
+  }
+
+  String GetZodiacSign() {
+    // var db = FirebaseDatabase.instance;
+    String dob = "06/08/1997";
+
+    int monthOfBirth;
+    int dayOfBirth;
+
+    var tmp = dob.split('/');
+    monthOfBirth = int.parse(tmp[0]);
+    dayOfBirth = int.parse(tmp[1]);
+
+    switch (monthOfBirth) {
+      case 1:
+        if (dayOfBirth > 20) {
+          return ZodiacSigns.CAPRICORN;
+        } else {
+          return ZodiacSigns.AQUARIUS;
+        }
+        break;
+      case 2:
+        if (dayOfBirth > 19) {
+          return ZodiacSigns.PISCES;
+        } else {
+          return ZodiacSigns.AQUARIUS;
+        }
+        break;
+      case 3:
+        if (dayOfBirth > 21) {
+          return ZodiacSigns.ARIES;
+        } else {
+          return ZodiacSigns.PISCES;
+        }
+        break;
+      case 4:
+        if (dayOfBirth > 20) {
+          return ZodiacSigns.TAURUS;
+        } else {
+          return ZodiacSigns.ARIES;
+        }
+        break;
+      case 5:
+        if (dayOfBirth > 21) {
+          return ZodiacSigns.GEMINI;
+        } else {
+          return ZodiacSigns.TAURUS;
+        }
+        break;
+      case 6:
+        if (dayOfBirth > 21) {
+          return ZodiacSigns.CANCER;
+        } else {
+          return ZodiacSigns.GEMINI;
+        }
+        break;
+      case 7:
+        if (dayOfBirth > 23) {
+          return ZodiacSigns.LEO;
+        } else {
+          return ZodiacSigns.CANCER;
+        }
+        break;
+      case 8:
+        if (dayOfBirth > 23) {
+          return ZodiacSigns.VIRGO;
+        } else {
+          return ZodiacSigns.LEO;
+        }
+        break;
+      case 9:
+        if (dayOfBirth > 23) {
+          return ZodiacSigns.LIBRA;
+        } else {
+          return ZodiacSigns.VIRGO;
+        }
+        break;
+      case 10:
+        if (dayOfBirth > 23) {
+          return ZodiacSigns.SCORPIO;
+        } else {
+          return ZodiacSigns.LIBRA;
+        }
+        break;
+      case 11:
+        if (dayOfBirth > 23) {
+          return ZodiacSigns.SAGITTARIUS;
+        } else {
+          return ZodiacSigns.SCORPIO;
+        }
+        break;
+      case 12:
+        if (dayOfBirth > 22) {
+          return ZodiacSigns.CAPRICORN;
+        } else {
+          return ZodiacSigns.SAGITTARIUS;
+        }
+        break;
+      default:
+        throw Exception("Error, invalid DOB");
+        break;
+    }
   }
 }
